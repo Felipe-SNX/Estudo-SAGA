8f54941f60e3262633c407474dc301ed89186393
unknown <felipesantossnx@gmail.com>
2025-10-18 08:33:33 -0300

    utilitários

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
@@ -0,0 +1,38 @@
+package br.com.microservices.orchestrated.inventoryservice.core.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class InventoryConsumer {
+
+    private final JsonUtil jsonUtil;
+
+    public InventoryConsumer(JsonUtil jsonUtil){
+        this.jsonUtil = jsonUtil;
+    }
+    
+    @KafkaListener(
+        groupId = "${spring.kafka.consumer.group-id}",
+        topics = "${spring.kafka.topic.payment-success}"
+    )
+    public void consumeSuccessEvent(String payload){
+        log.info("Receiving success event {} from payment-success topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+    @KafkaListener(
+        groupId = "${spring.kafka.consumer.group-id}",
+        topics = "${spring.kafka.topic.payment-fail}"
+    )
+    public void consumeFailEvent(String payload){
+        log.info("Receiving rollback event {} from payment-fail topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+    }
+
+}
---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/utils/JsonUtil.java
@@ -0,0 +1,35 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.utils;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.microservices.orchestrated.productvalidationservice.core.dto.Event;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class JsonUtil {
+    
+    private final ObjectMapper objectMapper;
+
+    public JsonUtil(ObjectMapper objectMapper){
+        this.objectMapper = objectMapper;
+    }
+
+    public String toJson(Object object){
+        try{
+            return objectMapper.writeValueAsString(object);
+        } catch(Exception ex){
+            return "";
+        }
+    }
+
+    public Event toEvent(String json){
+        try{
+            return objectMapper.readValue(json, Event.class);
+        } catch(Exception ex){
+            return null;
+        }
+    }
+}
